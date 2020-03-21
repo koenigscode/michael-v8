9ebf6e0a7fb96b1aa54600fd015c2d75ba638c87
Michael König <koenigmichael2002@gmail.com>
2020-03-21 12:32:24 +0100

    Finish about section

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,9 +1,54 @@
+const aboutEntries = [
+  {
+    title: "Hi!",
+    content: (
+      <p>
+        I'm Michael, 17 years old and currently attending the department for
+        Informatics at the{" "}
+        <a className="my-a" href="https://htlstp.ac.at/">
+          HTL St. Pölten
+        </a>{" "}
+        with focus on focus on Java, relational databases, network engineering
+        and project management.
+      </p>
+    )
+  },
+  {
+    title: "Programming",
+    content: (
+      <p>
+        Besides school I program a lot in my free time: web development with
+        modern js frameworks (react/next, vue), autonomous flying with drones
+        (python) and other projects.
+      </p>
+    )
+  },
+  {
+    title: "Creative",
+    content: (
+      <p>
+        I've been using Photoshop for several years now and enjoy editing photos
+        I've taken. I've also got some experience with Premiere Pro for editing
+        videos.
+      </p>
+    )
+  }
+]
+
 export default () => (
   <div className="About">
     <div className="About__content">
-      <h2 className="inline-block text-white text-3xl font-semibold">
-        // about me
-      </h2>
+      <h2 className="inline-block text-3xl font-semibold">// about me</h2>
+      <div className="flex justify-between mt-8">
+        {aboutEntries.map((entry, idx) => {
+          return (
+            <div key={idx} className="w-1/3 max-w-xs">
+              <h3 className="text-2xl font-semibold mb-4">{entry.title}</h3>
+              <div className="text-justify">{entry.content}</div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   </div>
 )

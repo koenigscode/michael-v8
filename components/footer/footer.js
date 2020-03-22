bd877fc2d4650ad8618fda73a2363c29fda92e44
Michael König <koenigmichael2002@gmail.com>
2020-03-22 16:43:29 +0100

    Add "contact me" to footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,8 +1,10 @@
+import Link from "next/link"
+
 export default () => (
   <div className="Footer">
-    <a href="/imprint" className="my-a mr-4">
-      Imprint
-    </a>
+    <Link href="/imprint">
+      <a className="my-a mr-4">Imprint</a>
+    </Link>
     <a href="mailto:koenigmichael2002@gmail.com" className="my-a">
       Contact Me
     </a>

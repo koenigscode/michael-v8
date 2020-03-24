ff46cb56e818dad09c7c136dd45350cfd30ead88
Michael König <koenigmichael2002@gmail.com>
2020-03-24 08:22:44 +0100

    Add _document.js and meta tags

--- components/head.js
+++ components/head.js
@@ -3,6 +3,12 @@ import Head from "next/head"
 export default () => (
   <Head>
     <title>Michael König</title>
+    <meta name="description" content="Personal Website of Michael König" />
+    <meta
+      name="keywords"
+      content="Michael König,Programming,ATRIA,Web Development"
+    />
+    <meta name="author" content="Michael König" />
     <link
       href="https://fonts.googleapis.com/css?family=Fira+Code:300,400,500,600,700&display=swap"
       rel="stylesheet"

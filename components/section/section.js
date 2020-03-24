49e9b83c3f7698e22b54bf38783496aa36cc30d0
Michael König <koenigmichael2002@gmail.com>
2020-03-24 11:16:16 +0100

    Fix svg

--- components/section/section.js
+++ components/section/section.js
@@ -17,17 +17,24 @@ class Section extends React.Component {
     return (
       <div>
         <style jsx>{`
-      .Section {
-        background: url("background_${this.props.dark ? "dark" : "light"}.svg");
-        color: ${this.props.dark ? "#fff" : "#222"};
-        margin-top: -${this.skewHeight};
-        padding-top: ${this.paddingTop};
-        padding-bottom: ${this.paddingBottom};
-        clip-path: polygon(0% 0%, 100% calc(${
-          this.skewHeight
-        } - 2px), 100% 100%, 0% 100%);
-      }
-    `}</style>
+          .Section {
+            background-color: ${this.props.dark ? "#222" : "#fff"};
+          background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 100 100'%3E%3Crect x='0' y='0' width='18' height='18' fill='%23${
+            this.props.dark ? "333333" : "cccccc"
+          }'/%3E%3C/svg%3E");
+
+            color: ${this.props.dark ? "#fff" : "#222"};
+            margin-top: -${this.skewHeight};
+            padding-top: ${this.paddingTop};
+            padding-bottom: ${this.paddingBottom};
+            clip-path: polygon(
+              0% 0%,
+              100% calc(${this.skewHeight} - 2px),
+              100% 100%,
+              0% 100%
+            );
+          }
+        `}</style>
         <div className="Section">
           <div className="Section__content">
             {this.props.heading && (

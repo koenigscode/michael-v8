f6807452a4363fcb1f75c530392b25aa9a9d598b
Michael König <koenigmichael2002@gmail.com>
2020-03-24 09:24:47 +0100

    Make pwa, add icons

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Imprint generated using https://www.firmenwebseiten.at/impressum-generator/,
 then stripped all 'class' and 'style' attributes off
+
+## Manifest
+
+Generated using https://app-manifest.firebaseapp.com/
--- components/head.js
+++ components/head.js
@@ -9,6 +9,36 @@ export default () => (
       content="Michael König,Programming,ATRIA,Web Development"
     />
     <meta name="author" content="Michael König" />
+    <meta name="theme-color" content="#ff4444" />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/images/favicons/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/images/favicons/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/images/favicons/favicon-16x16.png"
+    />
+    <link
+      rel="mask-icon"
+      href="/images/favicons/safari-pinned-tab.svg"
+      color="#212121"
+    />
+    <link rel="shortcut icon" href="/images/favicons/favicon.ico" />
+    <meta name="msapplication-TileColor" content="#ff4444" />
+    <meta
+      name="msapplication-config"
+      content="/images/favicons/browserconfig.xml"
+    />
+    <link rel="manifest" href="manifest.json"></link>
     <link
       href="https://fonts.googleapis.com/css?family=Fira+Code:300,400,500,600,700&display=swap"
       rel="stylesheet"
